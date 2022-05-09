ac2f423869f8c11bf70a56d5fc32fdc0246f04fc
OlgaDudnik <olga.dudnik.ua@gmail.com>
2022-05-09 18:26:41 +0300

    task-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const currentTimeKey = 'videoplayer-current-time';
+
+const player = new Player('handstick', {
+  id: 236203659,
+  width: 640,
+});
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    setVideoPlayerCurrentTime(data.seconds);
+  }, 1000),
+);
+
+if (getVideoPlayerCurrentTime() !== null) {
+  player.setCurrentTime(getVideoPlayerCurrentTime());
+}
+
+function setVideoPlayerCurrentTime(currentTime) {
+  localStorage.setItem(currentTimeKey, currentTime);
+}
+
+function getVideoPlayerCurrentTime() {
+  return localStorage.getItem(currentTimeKey);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import { throttle } from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector('input[name="email"]');
+const message = form.querySelector('textarea[name="message"]');
+const storageName = 'feedback-form-state';
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(storageName)));
+  form.reset();
+  localStorage.removeItem(storageName);
+});
+
+form.addEventListener('input', throttle(updateStorage, 500));
+
+function updateStorage() {
+  const formStorage = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem(storageName, JSON.stringify(formStorage));
+}
+
+function getEmailFromStorage() {
+  const storageData = localStorage.getItem(storageName);
+
+  if (storageData !== null) {
+    const storageDataObject = JSON.parse(storageData);
+    return storageDataObject.email;
+  }
+  return '';
+}
+
+function getMessageFromStorage() {
+  const storageData = localStorage.getItem(storageName);
+
+  if (storageData !== null) {
+    const storageDataObject = JSON.parse(storageData);
+    return storageDataObject.message;
+  }
+  return '';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  email.value = getEmailFromStorage();
+  message.value = getMessageFromStorage();
+});
